78df0cea42a99a9ff5ba39fe993ccaad5ba0a284
Jitse Goutbeek <147500739+JitseGoutbeek@users.noreply.github.com>
2024-06-28 11:20:23 +0200

    Update CheckingAccount.java
    Test voor Claude, zet het zo weer terug, ook om te zien hoe dit er op Sigrid uitziet

--- training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/CheckingAccount.java
+++ training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/CheckingAccount.java
@@ -1,36 +1,43 @@
 package eu.sig.training.ch04;
 
-// tag::CheckingAccount[]
 public class CheckingAccount {
     private static final float INTEREST_PERCENTAGE = 0.01f;
     private Money balance = new Money();
     private int transferLimit = 100;
 
-    public Transfer makeTransfer(String counterAccount, Money amount)
-        throws BusinessException {
-        // 1. Check withdrawal limit:
+    public Transfer makeTransfer(String counterAccount, Money amount) throws BusinessException {
+        validateTransferAmount(amount);
+        validateAccountNumber(counterAccount);
+        return createTransfer(counterAccount, amount);
+    }
+
+    private void validateTransferAmount(Money amount) throws BusinessException {
         if (amount.greaterThan(this.transferLimit)) {
             throw new BusinessException("Limit exceeded!");
         }
-        // 2. Assuming result is 9-digit bank account number, validate 11-test:
-        int sum = 0;
-        for (int i = 0; i < counterAccount.length(); i++) {
-            char character = counterAccount.charAt(i);
-            int characterValue = Character.getNumericValue(character);
-            sum = sum + (9 - i) * characterValue;
-        }
-        if (sum % 11 == 0) {
-            // 3. Look up counter account and make transfer object:
-            CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
-            Transfer result = new Transfer(this, acct, amount);
-            return result;
-        } else {
+    }
+
+    private void validateAccountNumber(String accountNumber) throws BusinessException {
+        if (!AccountValidator.isValid(accountNumber)) {
             throw new BusinessException("Invalid account number!");
         }
     }
 
+    private Transfer createTransfer(String counterAccount, Money amount) {
+        CheckingAccount acct = Accounts.findAcctByNumber(counterAccount);
+        return new Transfer(this, acct, amount);
+    }
+
     public void addInterest() {
-        Money interest = balance.multiply(INTEREST_PERCENTAGE);
+        Money interest = calculateInterest();
+        updateBalance(interest);
+    }
+
+    private Money calculateInterest() {
+        return balance.multiply(INTEREST_PERCENTAGE);
+    }
+
+    private void updateBalance(Money interest) {
         if (interest.greaterThan(0)) {
             balance.add(interest);
         } else {
@@ -38,4 +45,14 @@ public class CheckingAccount {
         }
     }
 }
-// end::CheckingAccount[]
+
+class AccountValidator {
+    public static boolean isValid(String accountNumber) {
+        int sum = 0;
+        for (int i = 0; i < accountNumber.length(); i++) {
+            int digitValue = Character.getNumericValue(accountNumber.charAt(i));
+            sum += (9 - i) * digitValue;
+        }
+        return sum % 11 == 0;
+    }
+}

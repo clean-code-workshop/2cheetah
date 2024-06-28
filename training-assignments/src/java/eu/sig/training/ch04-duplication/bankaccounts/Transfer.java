cb18c993e0bb8f8924e0d9d53b483fe79cf43cb0
Jitse Goutbeek <147500739+JitseGoutbeek@users.noreply.github.com>
2024-06-28 12:35:05 +0200

    Update Transfer.java

--- training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/Transfer.java
+++ training-assignments/src/java/eu/sig/training/ch04-duplication/bankaccounts/Transfer.java
@@ -1,26 +1,61 @@
 package eu.sig.training.ch04;
 
 public class Transfer {
+    private final TransferDetails details;
+
+    public Transfer(TransferDetails details) {
+        this.details = details;
+    }
+
+    public Account getSourceAccount() {
+        return details.getSourceAccount();
+    }
+
+    public CheckingAccount getDestinationAccount() {
+        return details.getDestinationAccount();
+    }
+
+    public Money getAmount() {
+        return details.getAmount();
+    }
+}
+
+public class TransferDetails {
     private final Account sourceAccount;
     private final CheckingAccount destinationAccount;
     private final Money amount;
 
-    public Transfer(Account sourceAccount, CheckingAccount destinationAccount, Money amount) {
-        this.sourceAccount = sourceAccount;
-        this.destinationAccount = destinationAccount;
-        this.amount = amount;
+    private TransferDetails(Builder builder) {
+        this.sourceAccount = builder.sourceAccount;
+        this.destinationAccount = builder.destinationAccount;
+        this.amount = builder.amount;
     }
 
-    public Account getSourceAccount() {
-        return sourceAccount;
-    }
+    // Getters...
 
-    public CheckingAccount getDestinationAccount() {
-        return destinationAccount;
-    }
+    public static class Builder {
+        private Account sourceAccount;
+        private CheckingAccount destinationAccount;
+        private Money amount;
 
-    public Money getAmount() {
-        return amount;
+        public Builder sourceAccount(Account sourceAccount) {
+            this.sourceAccount = sourceAccount;
+            return this;
+        }
+
+        public Builder destinationAccount(CheckingAccount destinationAccount) {
+            this.destinationAccount = destinationAccount;
+            return this;
+        }
+
+        public Builder amount(Money amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public TransferDetails build() {
+            return new TransferDetails(this);
+        }
     }
 }
 
